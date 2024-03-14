9a4d7c49cd98c81e006ab44c7f171e46c941a533
Ramsha Shoaib <159899975+ramshaaaa5@users.noreply.github.com>
2024-03-14 20:28:02 +0500

    Add files via upload

---
+++ 19. Dinner Guests/app.js
@@ -0,0 +1,3 @@
+/*Question # 19: Dinner Guests: Print a message indicating the number of people you are inviting to dinner.*/
+var guests = ["Aisha", "Maria", "Sara", "Alisha", "Wania"];
+console.log("I am inviting ".concat(guests.length, " people to dinner."));
---
+++ 19. Dinner Guests/app.ts
@@ -0,0 +1,4 @@
+/*Question # 19: Dinner Guests: Print a message indicating the number of people you are inviting to dinner.*/
+
+let guests: string[] = ["Aisha", "Maria", "Sara", "Alisha", "Wania"]
+console.log(`I am inviting ${guests.length} people to dinner.`)
