e5643974cda879155278b948a9bb432cf05982f2
Ramsha Shoaib <159899975+ramshaaaa5@users.noreply.github.com>
2024-03-14 20:14:49 +0500

    Add files via upload

---
+++ 09. Favorite Number/app.js
@@ -0,0 +1,4 @@
+/*Question # 9:Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message
+that reveals your favorite number. Print that message.*/
+var favoriteNumber = 5;
+console.log("My favourite number is ".concat(favoriteNumber, "."));
---
+++ 09. Favorite Number/app.ts
@@ -0,0 +1,5 @@
+/*Question # 9:Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message 
+that reveals your favorite number. Print that message.*/
+
+let favoriteNumber = 5
+console.log(`My favourite number is ${favoriteNumber}.`)
