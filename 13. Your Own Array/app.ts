fd315045f0909da40f43ecf8affbbe956979bd3b
Ramsha Shoaib <159899975+ramshaaaa5@users.noreply.github.com>
2024-03-14 20:20:44 +0500

    Add files via upload

---
+++ 13. Your Own Array/app.ts
@@ -0,0 +1,8 @@
+/*Question # 13: Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, 
+and make a list that stores several examples. Use your list to print a series of statements about these items, 
+such as “I would like to own a Honda motorcycle.”*/
+
+let transport: string[] = ["Honda motorcycle", "Tesla car", "Bianchi bicycle"]
+transport.forEach(transport => {
+    console.log(`I would like to own a ${transport}.`)
+})
