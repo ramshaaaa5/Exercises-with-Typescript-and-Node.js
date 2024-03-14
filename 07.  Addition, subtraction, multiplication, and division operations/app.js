c1daad9ddbe4c06e63ef2e4f31e90b5e81f55651
Ramsha Shoaib <159899975+ramshaaaa5@users.noreply.github.com>
2024-03-14 20:13:24 +0500

    Add files via upload

---
+++ 07.  Addition, subtraction, multiplication, and division operations/app.js
@@ -0,0 +1,22 @@
+/*Question # 7: Number Eight: Write addition, subtraction, multiplication, and division operations that each result
+in the number 8. Be sure to enclose your operations in print statements to see the results.*/
+//addition operation
+var a = 6;
+var b = 2;
+var sum = a + b;
+console.log("Addition operation: ".concat(a, "+").concat(b, "=").concat(sum));
+//subtraction operation
+var c = 12;
+var d = 4;
+var sub = c - d;
+console.log("Subtraction operation:".concat(c, "-").concat(d, "=").concat(sub));
+//multiplication operation
+var e = 2;
+var f = 4;
+var mul = e * f;
+console.log("Multiplication operation: ".concat(e, "*").concat(f, "=").concat(mul));
+//division operation
+var g = 32;
+var h = 4;
+var div = g / h;
+console.log("Division operation: ".concat(g, "/").concat(h, "=").concat(div));
---
+++ 07.  Addition, subtraction, multiplication, and division operations/app.ts
@@ -0,0 +1,23 @@
+/*Question # 7: Number Eight: Write addition, subtraction, multiplication, and division operations that each result 
+in the number 8. Be sure to enclose your operations in print statements to see the results.*/
+
+//addition operation
+let a:number = 6
+let b:number = 2
+let sum:number = a+b
+console.log(`Addition operation: ${a}+${b}=${sum}`)
+//subtraction operation
+let c:number = 12
+let d:number = 4
+let sub:number = c-d
+console.log(`Subtraction operation:${c}-${d}=${sub}`)
+//multiplication operation
+let e:number = 2
+let f:number = 4
+let mul:number = e*f
+console.log(`Multiplication operation: ${e}*${f}=${mul}`)
+//division operation
+let g:number = 32
+let h:number = 4
+let div:number = g/h
+console.log(`Division operation: ${g}/${h}=${div}`)
