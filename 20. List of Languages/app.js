c7ab70e31100b6fef069d5030c9f1f76af7db5d2
Ramsha Shoaib <159899975+ramshaaaa5@users.noreply.github.com>
2024-03-14 20:30:04 +0500

    Add files via upload

---
+++ 20. List of Languages/app.js
@@ -0,0 +1,4 @@
+/*Question # 20: Think of something you could store in a array. For example, you could make a list of mountains, rivers,
+countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.*/
+var language = ["English", "Urdu", "French", "German", "Arabic"];
+console.log("Languages I'd like to learn:", language);
