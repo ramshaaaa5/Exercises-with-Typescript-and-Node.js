306145b30d46e7405ea795d7237de4ecce3d4e38
Ramsha Shoaib <159899975+ramshaaaa5@users.noreply.github.com>
2024-03-15 23:39:46 +0500

    Add files via upload

---
+++ 32. Checking Usernames/app.js
@@ -0,0 +1,13 @@
+// Question # 32: Checking Usernames
+var current_users = ["user1", "admin", "user3", "user4", "user5"];
+var new_users = ["User1", "user6", "user7", "admin", "user9"];
+new_users.forEach(function (newUser) {
+    if (current_users.some(function (currentUser) {
+        return currentUser.toLowerCase() === newUser.toLowerCase();
+    })) {
+        console.log("".concat(newUser, " will need to enter a new username."));
+    }
+    else {
+        console.log("".concat(newUser, " is available."));
+    }
+});
