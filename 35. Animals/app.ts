fb3c6c217663986d914a5bdbcf50aad31f11a9c1
Ramsha Shoaib <159899975+ramshaaaa5@users.noreply.github.com>
2024-03-15 23:44:04 +0500

    Add files via upload

---
+++ 35. Animals/app.ts
@@ -0,0 +1,7 @@
+// Question # 35: Animals
+
+let animals: string[] = ["dog", "cat", "rabbit"]
+animals.forEach(animal => {
+    console.log(`A ${animal} would make a great pet.`)
+})
+console.log("Any of these animals would make a great pet!");
