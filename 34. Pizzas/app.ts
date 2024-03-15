8e6c5207997382d7505157fc996c4bcfbee9edc4
Ramsha Shoaib <159899975+ramshaaaa5@users.noreply.github.com>
2024-03-15 23:42:27 +0500

    Add files via upload

---
+++ 34. Pizzas/app.ts
@@ -0,0 +1,7 @@
+// Question # 34: Pizzas
+
+let pizzas: string[] = ["pepporoni", "margherita", "hawaiian"]
+pizzas.forEach(pizza => {
+    console.log(`I like ${pizza} pizza.`);
+})
+console.log("I really love pizza!");
