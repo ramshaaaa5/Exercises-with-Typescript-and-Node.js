9389ce8d4f5f8080d1e1ae2710b085fb8687e8ef
Ramsha Shoaib <159899975+ramshaaaa5@users.noreply.github.com>
2024-03-15 23:17:08 +0500

    Add files via upload

---
+++ 23. Conditional Tests/app.js
@@ -0,0 +1,20 @@
+/* Question # 23: Conditional Tests: Write a series of conditional tests. Print a statement describing each test and
+your prediction for the results of each test.*/
+var car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru'); //True
+console.log("Is car == 'toyota'? I predict False.");
+console.log(car == 'toyota'); //False
+console.log("Is car != 'toyota'? I predict true.");
+console.log(car != 'toyota'); //true
+console.log("Is car != 'subaru'? I predict false.");
+console.log(car != 'subaru'); //false
+var color = 'red';
+console.log("Is color == 'red'? I predict True.");
+console.log(color == 'red'); //True
+console.log("Is color == 'blue'? I predict False.");
+console.log(color == 'blue'); //False
+console.log("Is color != 'blue'? I predict true.");
+console.log(color != 'blue'); //true
+console.log("Is color != 'red'? I predict false.");
+console.log(color != 'red'); //false
