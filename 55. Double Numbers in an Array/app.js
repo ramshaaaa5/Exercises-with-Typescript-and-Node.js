b16b6faa88f3caa656dd775295665ceb7f220747
Ramsha Shoaib <159899975+ramshaaaa5@users.noreply.github.com>
2024-03-26 23:44:07 +0500

    Add files via upload

---
+++ 55. Double Numbers in an Array/app.js
@@ -0,0 +1,4 @@
+//Question # 55. Double Numbers in an Array
+var numbers = [1, 2, 3, 4, 5];
+var doubleNumbers = numbers.map(function (number) { return number * 2; });
+console.log(doubleNumbers);
---
+++ 55. Double Numbers in an Array/app.ts
@@ -0,0 +1,7 @@
+//Question # 55. Double Numbers in an Array
+
+let numbers= [1, 2, 3, 4, 5]
+
+let doubleNumbers = numbers.map(number => number * 2)
+
+console.log(doubleNumbers);
