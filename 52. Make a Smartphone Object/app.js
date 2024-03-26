5b45dacfbec82b1be2f7b53fbe46909d5fd0c145
Ramsha Shoaib <159899975+ramshaaaa5@users.noreply.github.com>
2024-03-26 23:23:40 +0500

    Add files via upload

---
+++ 52. Make a Smartphone Object/app.js
@@ -0,0 +1,11 @@
+// Question # 52. Make a Smartphone Object
+var smartphone = {
+    make: "Samsung",
+    model: "Galaxy S21",
+    specs: {
+        storage: "128GB",
+        screen_size: "6.2 inches",
+        battery_life: "18 hours"
+    }
+};
+console.log(smartphone);
---
+++ 52. Make a Smartphone Object/app.ts
@@ -0,0 +1,13 @@
+// Question # 52. Make a Smartphone Object
+
+let smartphone = {
+    make: "Samsung",
+    model: "Galaxy S21",
+    specs: {
+        storage: "128GB",
+        screen_size: "6.2 inches",
+        battery_life: "18 hours"
+    }
+}
+
+console.log(smartphone);
