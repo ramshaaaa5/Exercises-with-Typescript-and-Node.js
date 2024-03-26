0d066db5bb2abcd828e39c6a3f41a1bf6aa70700
Ramsha Shoaib <159899975+ramshaaaa5@users.noreply.github.com>
2024-03-26 23:52:48 +0500

    Add files via upload

---
+++ 64. Combine Strings and Numbers & 65. Determine the Remainder/app.js
@@ -0,0 +1,10 @@
+//Question # 64. Combine Strings and Numbers
+function combineStringAndNumber(text, number) {
+    return text + number;
+}
+console.log(combineStringAndNumber("Age:", 30));
+//Question # 65. Determine the Remainder
+function remainder(num1, num2) {
+    return num1 % num2;
+}
+console.log(remainder(5, 2));
---
+++ 64. Combine Strings and Numbers & 65. Determine the Remainder/app.ts
@@ -0,0 +1,14 @@
+//Question # 64. Combine Strings and Numbers
+
+function combineStringAndNumber(text:string, number:number):string {
+    return text + number
+}
+
+console.log(combineStringAndNumber("Age:", 30));
+
+//Question # 65. Determine the Remainder
+
+function remainder(num1: number, num2: number):number {
+    return num1 % num2 
+}
+console.log(remainder(5,2));
