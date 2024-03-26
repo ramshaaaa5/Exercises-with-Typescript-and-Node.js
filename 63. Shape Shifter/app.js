7c9305656f611043014d487e01f3f9af8ce4166e
Ramsha Shoaib <159899975+ramshaaaa5@users.noreply.github.com>
2024-03-26 23:50:26 +0500

    Add files via upload

---
+++ 63. Shape Shifter/app.js
@@ -0,0 +1,12 @@
+//Question # 63. Shape Shifter
+var circle = {
+    kind: "circle",
+    radius: 5
+};
+var rectangle = {
+    kind: "rectangle",
+    width: 10,
+    height: 20
+};
+console.log(circle);
+console.log(rectangle);
