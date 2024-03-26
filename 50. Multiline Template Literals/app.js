068a2a2373be4f8c55ec100f19cf7cfd5bec16eb
Ramsha Shoaib <159899975+ramshaaaa5@users.noreply.github.com>
2024-03-26 16:34:41 +0500

    Add files via upload

---
+++ 50. Multiline Template Literals/app.js
@@ -0,0 +1,3 @@
+//Question # 50. Multiline Template Literals
+var myIdealDay = "My ideal day would involve:\n1. Waking up early and going for ajog.\n2. Spend a few hours coding on a personal project.\n3. Ending the day by reading a good book.";
+console.log(myIdealDay);
---
+++ 50. Multiline Template Literals/app.ts
@@ -0,0 +1,8 @@
+//Question # 50. Multiline Template Literals
+
+let myIdealDay = `My ideal day would involve:
+1. Waking up early and going for ajog.
+2. Spend a few hours coding on a personal project.
+3. Ending the day by reading a good book.`
+
+console.log(myIdealDay);
