1cc3fffd68741bb98b98e43a4b34df8c37087926
Ramsha Shoaib <159899975+ramshaaaa5@users.noreply.github.com>
2024-04-22 14:29:57 +0500

    Add files via upload

---
+++ 79. Creating and Accessing Object Properties & 80. Updating Object Properties/app.ts
@@ -0,0 +1,18 @@
+//Question 79: Creating and Accessing Object Properties
+
+let car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020,
+    color: ""
+}
+
+console.log(car.model);
+
+
+//Question 80. Updating Object Properties
+
+car.color = "blue"
+car.year = 2021; 
+
+console.log(car);
